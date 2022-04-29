9d6354700d4262da2b3d6accaee616a27dfbd7e0
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-29 15:23:19 +0600

    Middlewares added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
