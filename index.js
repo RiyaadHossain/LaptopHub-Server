0ae63e84867f4c6e15716c9443ed736e5833d340
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-30 10:22:54 +0600

    Basic get operation done

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
-const port = process.env.PORT || 4000
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 4000;
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@laptopwarehouse.alehg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const laptopCollection = client.db("AllLaptop").collection("Laptop");
+
+    app.get("/laptops", async (req, res) => {
+      const result = await laptopCollection.find({}).toArray();
+
+      res.send(result);
+    });
+
+    app.post("/laptop", async (req, res) => {});
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
